ca2fd64242e473fb7e3de961c0b27d6569c1586c
Yunpei Liu <812556867@qq.com>
2022-05-13 20:37:59 +0800

    add check_coverage for ClusterReactionWorkflow; add new calc-code structure

---
+++ miko_tasker/code/base.py
@@ -0,0 +1,39 @@
+import os
+import yaml
+from abc import ABCMeta, abstractmethod
+
+
+class BaseRender(metaclass=ABCMeta):
+    @abstractmethod
+    def from_file(self, path):
+        pass
+
+    @abstractmethod
+    def make_file(self, path):
+        pass
+
+
+class TemplateRender(BaseRender):
+
+    def __init__(self, render_dict):
+        self.render_dict = render_dict
+        self.template = None
+
+    def from_file(self, template_file):
+        from jinja2 import FileSystemLoader, Environment
+        loader = FileSystemLoader(os.path.abspath(template_file))
+        env = Environment(loader=loader)
+        self.template = env.get_template('demo.html')
+
+    def load_render_dict(self, render_config):
+        with open(os.path.abspath(render_config)) as f:
+            self.render_dict = yaml.safe_load(f)
+
+    def render_template(self):
+        pass
+
+    def make_file(self):
+        pass
+
+
+
---
+++ miko_tasker/code/vasp.py
@@ -0,0 +1,9 @@
+from .base import BaseRender
+
+
+class VaspRender(BaseRender):
+    def from_file(self, path):
+        pass
+
+    def make_file(self, path):
+        pass
