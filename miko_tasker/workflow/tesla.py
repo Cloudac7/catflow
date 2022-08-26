33cd0b63b50fd4829f5ccc263f7e96d389c805a2
Cloudac7 <812556867@qq.com>
2022-08-26 16:03:35 +0800

    fix more bug

--- miko_tasker/workflow/tesla.py
+++ miko_tasker/workflow/tesla.py
@@ -154,7 +154,7 @@ class CLWorkFlow(object):
 
     def run_step(self):
         logger.info("now stage: {0}, step: {1}".format(
-                self.stage, self.step))
+            self.stage, self.step))
         logger.info("now main step: {}".format(self.main_step))
         logger.info("now real step: {}".format(self.real_step))
         stage_class = self.get_work_step(self.main_step)
@@ -191,7 +191,7 @@ class CLWorkFlow(object):
         if param_file == None:
             param_file = self.param_file
             shutil.copyfile(
-                self.param_file, 
+                self.param_file,
                 self.param_file.parent / f"params_backup_{self.stage}.json"
             )
         with open(param_file, "w", encoding='utf-8') as f:
@@ -215,32 +215,41 @@ class ClusterReactionWorkflow(CLWorkFlow):
         # check before model_devi step start
         if self.main_step == 1:
             if self.real_step == 0:
-                last_model_devi_job = self.params.get('model_devi_jobs')[-1]
+                model_devi_jobs = self.params.get('model_devi_jobs')
+                try:
+                    last_model_devi_job = model_devi_jobs[-1]
+                except IndexError:
+                    last_model_devi_job = {}
                 last_sys_idx = last_model_devi_job.get('sys_idx')
-                conv_flags = np.zeros_like(last_sys_idx, dtype=int)
-                for i, idx in enumerate(last_sys_idx):
-                    logger.info(
-                        f'Checking convergancy for iteration {self.stage}')
-                    accu_ratio = self._check_index_converge(idx)
-                    logger.info(
-                        f'idx {idx} reach accuracy ratio: {accu_ratio}')
-                    if accu_ratio >= 0.97:
-                        conv_flags[i] = 1
-                if 1 in conv_flags:
-                    # not all accu, update params
-                    logger.info('Not all idxs reach 97% accuracy')
-                    logger.info('Continue training process')
-                    self.update_params()
-                    return True
-                elif self.stage <= self.workflow_settings.get('start_from_iter', 0):
-                    # prevent not starting
-                    logger.info('First iteration')
+                if len(last_sys_idx) == 0:
+                    logger.info('Empty model_devi_job found')
                     self.update_params()
                     return True
                 else:
-                    # all accu, finish exploration
-                    logger.info('Model accuracy converged.')
-                    return False
+                    conv_flags = np.zeros_like(last_sys_idx, dtype=int)
+                    logger.info(
+                            f'Checking convergency for iteration {self.stage}')
+                    for i, idx in enumerate(last_sys_idx):
+                        accu_ratio = self._check_index_converge(idx)
+                        logger.info(
+                            f'idx {idx} reach accuracy ratio: {round(accu_ratio, 3)}')
+                        if accu_ratio >= 0.97:
+                            conv_flags[i] = 1
+                    if 0 in conv_flags:
+                        # not all accu, update params
+                        logger.info('Not all idxs reach 97% accuracy')
+                        logger.info('Continue training process')
+                        self.update_params()
+                        return True
+                    elif self.stage <= self.workflow_settings.get('start_from_iter', 0):
+                        # prevent not starting
+                        logger.info('This is the first iteration')
+                        self.update_params()
+                        return True
+                    else:
+                        # all accu, finish exploration
+                        logger.info('Model accuracy converged.')
+                        return False
             else:
                 return True
         else:
@@ -279,7 +288,11 @@ class ClusterReactionWorkflow(CLWorkFlow):
         candidate_count = self._check_shuffled_log(
             f"candidate.shuffled.{str(index).zfill(3)}.out")
         all_count = accu_count + failed_count + candidate_count
-        accu_ratio = accu_count / all_count
+        try:
+            accu_ratio = accu_count / all_count
+        except ZeroDivisionError:
+            logger.info("no exploration task for idx {}".format(index))
+            accu_ratio = 1.
         return accu_ratio
 
     def _check_shuffled_log(self, shuffled_logs):
@@ -297,19 +310,16 @@ class ClusterReactionWorkflow(CLWorkFlow):
         return log_count
 
     def update_params(self):
-        # TODO: update self.params, generating new model_devi_jobs
-        if len(self.params['model_devi_jobs']) < self.stage + 1:
-            self.params['model_devi_jobs'].append(
-                self.params['model_devi_jobs'][-1])
+        updater = ClusterReactionUpdater(self)
+        updater.model_devi_job_generator()
+        self._set_cur_trust_level()
+        self.render_params()
 
+    def _set_cur_trust_level(self):
         f_trust_lo, f_trust_hi = self.guess_trust_level()
         cur_job = self.params['model_devi_jobs'][self.stage]
         cur_job["model_devi_f_trust_lo"] = f_trust_lo
         cur_job["model_devi_f_trust_hi"] = f_trust_hi
-        # 第二轮开始往训练里加新的结构并针对性探索
-        updater = ClusterReactionUpdater(self)
-        updater.model_devi_job_generator()
-        self.render_params()
 
     def guess_trust_level(self, stage=None):
         """guess trust level from training step each iteration
@@ -328,7 +338,7 @@ class ClusterReactionWorkflow(CLWorkFlow):
         try:
             training_l2_error = np.loadtxt(
                 Path(f"iter.{str(stage).zfill(6)}/00.train/000/lcurve.out").resolve(), usecols=3)
-            mean_l2_error = np.mean(training_l2_error[-25:])
+            mean_l2_error = np.mean(training_l2_error[-int(len(training_l2_error)/10):])
             logger.info("Use values guessed from training.")
         except FileNotFoundError:
             logger.info("Training task not found. Use default values.")
@@ -341,13 +351,15 @@ class ClusterReactionWorkflow(CLWorkFlow):
 class ClusterReactionUpdater:
     """update the params during each iteration loop
     """
+
     def __init__(self, wf: ClusterReactionWorkflow):
         self.workflow = wf
 
     def model_devi_job_generator(self):
         while len(self.workflow.params['model_devi_jobs']) < self.workflow.stage + 1:
             logger.debug(f"Add new model_devi_job.")
-            self.workflow.params['model_devi_jobs'].append(self.workflow.params['model_devi_jobs'][-1])
+            self.workflow.params['model_devi_jobs'].append(
+                self.workflow.params['model_devi_jobs'][-1])
         cur_job = self.workflow.params['model_devi_jobs'][self.workflow.stage]
         self._new_template_generator(cur_job)
 
@@ -362,10 +374,12 @@ class ClusterReactionUpdater:
     def exploration_track(self):
         is_coord = self.workflow.IS['coordination']
         fs_coord = self.workflow.FS['coordination']
-        full_track = np.arange(is_coord, fs_coord + self.exploration_step, self.exploration_step)
+        full_track = np.arange(is_coord, fs_coord +
+                               self.exploration_step, self.exploration_step)
         return full_track
 
     def _new_template_generator(self, cur_job):
+        cur_job["_idx"] = str(self.workflow.stage)
         # get IS and FS sys_idx and coordination from self.workflow.workflow_settings
         is_sys_idx, is_coord = self._get_cv_setting('IS')
         fs_sys_idx, fs_coord = self._get_cv_setting('FS')
@@ -407,16 +421,23 @@ class ClusterReactionUpdater:
             else:
                 for key in cur_job['sys_rev_mat'].keys():
                     cur_job['sys_rev_mat'][key]['lmp']['V_DIS2'] = cur_job['sys_rev_mat'][key]['lmp']['V_DIS1']
-                
+
+                task_list, distances = self._distances()
+
                 is_coords = [i['lmp']['V_DIS1'][0]
                              for i in cur_job['sys_rev_mat'].values() if i.get('_type') == 'IS']
-                logger.info(f"exploring IS coords: {is_coords}")
+                logger.info(f"explored IS coords: {is_coords}")
 
-                for new_coord in [max(is_coords) + exploration_step, max(is_coords) + 2 * exploration_step]:
+                for new_coord in [
+                    round(max(is_coords) + exploration_step, 3),
+                    round(max(is_coords) + 2 * exploration_step, 3)
+                ]:
                     if new_coord < self.exploration_track[center_idx]:
-                        is_sys_idx = self._pick_new_structure(max(is_coords) + exploration_step)
+                        is_sys_idx = self._pick_new_structure(new_coord, task_list, distances)
                         logger.debug(f"add new sys_idx: {is_sys_idx}")
                         if is_sys_idx is not None:
+                            logger.info(
+                                f"add new exploration coord: {new_coord}")
                             cur_job["sys_idx"].append(is_sys_idx)
                             cur_job["sys_rev_mat"][str(is_sys_idx)] = {
                                 "lmp": {
@@ -429,12 +450,17 @@ class ClusterReactionUpdater:
 
                 fs_coords = [i['lmp']['V_DIS1'][0]
                              for i in cur_job['sys_rev_mat'].values() if i.get('_type') == 'FS']
-                logger.info(f"exploring FS coords: {fs_coords}")
+                logger.info(f"explored FS coords: {fs_coords}")
 
-                for new_coord in [min(fs_coords) - exploration_step, min(fs_coords) - 2 * exploration_step]:
+                for new_coord in [
+                    round(min(fs_coords) - exploration_step, 3), 
+                    round(min(fs_coords) - 2 * exploration_step, 3)
+                ]:
                     if new_coord >= self.exploration_track[center_idx]:
-                        fs_sys_idx = self._pick_new_structure(min(fs_coords) + exploration_step)
+                        fs_sys_idx = self._pick_new_structure(new_coord, task_list, distances)
                         if fs_sys_idx is not None:
+                            logger.info(
+                                f"add new exploration coords: {new_coord}")
                             cur_job["sys_idx"].append(fs_sys_idx)
                             cur_job["sys_rev_mat"][str(fs_sys_idx)] = {
                                 "lmp": {
@@ -445,28 +471,36 @@ class ClusterReactionUpdater:
                                 "_type": "FS"
                             }
 
-    def _pick_new_structure(self, new_start_coord):
-        task_list = sorted(self.workflow.work_path.glob('iter.*/02.fp/task.*.*/POSCAR'))
+    def _distances(self):
+        task_list = []
+        for i in range(self.workflow.workflow_settings['start_from_iter'], self.workflow.stage):
+            task_list += sorted((self.workflow.work_path / f"iter.{str(i).zfill(6)}").glob(
+                '02.fp/task.*.*/POSCAR'))
         distances = []
         for i in task_list:
             _s = read(i)
-            distances.append(_s.get_distance(self.workflow.reaction_atoms_pair))
+            distances.append(_s.get_distance(
+                *self.workflow.reaction_atoms_pair))
+        return task_list, distances
+
+    def _pick_new_structure(self, new_start_coord, task_list, distances):
         try:
             new_idx = np.where((np.array(distances) >= new_start_coord - 0.05)
                                & (np.array(distances) < new_start_coord + 0.05))[0][0]
         except IndexError:
             logger.info("New structure not found!")
             return None
-        new_structure_path = Path(task_list[new_idx]) / "POSCAR"
-        new_sys_idx = self.workflow._render_new_system(new_structure_path)
+        new_structure_path = Path(task_list[new_idx])
+        new_sys_idx = self._render_new_system(new_structure_path)
         return new_sys_idx
 
     def _render_new_system(self, new_structure_path: Path):
         sys_configs_prefix = self.workflow.params["sys_configs_prefix"]
-        new_sys_item = [new_structure_path.relative_to(sys_configs_prefix)]
+        new_sys_item = [
+            str(new_structure_path.relative_to(sys_configs_prefix))]
         self.workflow.params["sys_configs"].append(new_sys_item)
         self.workflow.params["sys_batch_size"].append("auto")
-        return len(self.workflow.params["sys_configs"])
+        return len(self.workflow.params["sys_configs"]) - 1
 
     def _get_cv_setting(self, cv_type):
         sys_idx = self.workflow.workflow_settings[cv_type]['sys_idx']
