269969d0bf4e736ad879e40a173a74108145fdc3
Cloudac7 <812556867@qq.com>
2022-08-29 20:18:40 +0800

    generalize miko.cmdline entry point

--- miko/cmdline/base.py
+++ miko/cmdline/base.py
@@ -4,11 +4,12 @@ from importlib.metadata import entry_points
 
 from .simu import simu_cli
 from .fprun import fprun_cli
-from .tasker import tasker_cli
 
+
+miko_eps = entry_points().get('miko.cmdline', [])
 
 cli = click.CommandCollection(sources=[
     simu_cli,
     fprun_cli,
-    tasker_cli
+    *miko_eps
 ])
