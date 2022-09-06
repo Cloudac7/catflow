f00238f954ff010e78bf606122763d6a152f7299
Cloudac7 <812556867@qq.com>
2022-09-06 17:24:59 +0800

    fix bug for cmdline

--- miko/cmdline/base.py
+++ miko/cmdline/base.py
@@ -7,9 +7,13 @@ from .fprun import fprun_cli
 
 
 miko_eps = entry_points().get('miko.cmdline', [])
+if miko_eps != []:
+    miko_eps_cli = [i.load() for i in miko_eps]
+else:
+    miko_eps_cli =[]
 
 cli = click.CommandCollection(sources=[
     simu_cli,
     fprun_cli,
-    *miko_eps
+    *miko_eps_cli
 ])
